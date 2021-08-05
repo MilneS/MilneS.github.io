a8343e5b7a93ae6fa201c04777d88fef171b792d
Samira Milne <milne.samira@GMAIL.COM>
2021-08-05 14:29:38 -0500

    map through projectsdata

--- src/comps/Portfolio.js
+++ src/comps/Portfolio.js
@@ -1,13 +1,7 @@
 import classes from "./Portfolio.module.css";
 import Modal from "./Modal";
 import { useState } from "react";
-
-import foodOrder from "./projectspics/foodorder.png";
-import meetups from "./projectspics/meetups.png";
-import quotes from "./projectspics/quotes.png";
-import expensesChart from "./projectspics/expenseschart.png";
-import colorGame from "./projectspics/colorgame.png";
-import yelpCamp from "./projectspics/yelpcamp.png";
+import ProjectsData from "./Projectsdata";
 
 const Portfolio = () => {
   const [showModal, setShowModal] = useState(false);
@@ -19,7 +13,6 @@ const Portfolio = () => {
     }
   };
 
-
   return (
     <div className={classes.containerright} id="portfolio">
       <div>
@@ -27,93 +20,24 @@ const Portfolio = () => {
       </div>
       <div>
         <ul className={classes.projectscontainer}>
-          <li>
-            <div className={classes.imgcontainer}>
-              <img src={foodOrder} alt="" />
-              <div className={classes.after}>
-                <p onClick={modalHandler} className={classes.view}>
-                  View
-                </p>
+          {ProjectsData.map((item) => (
+            <li key={item.id}>
+              <div className={classes.imgcontainer}>
+                <img src={item.pic} alt="" />
+                <div className={classes.after}>
+                  <p onClick={modalHandler} className={classes.view}>
+                    View
+                  </p>
+                </div>
+                {showModal && <Modal closeModal={modalHandler} />}
               </div>
-              {showModal && (
-                  <Modal closeModal={modalHandler} />
-                )}
-            </div>
-            <div>
-              <i className="fa fa-github" />
-              <a href="https://github.com/MilneS/food-order-app">
-                Food Order app
-              </a>
-            </div>
-            <div>React</div>
-          </li>
-          <li>
-            <div className={classes.imgcontainer}>
-              <img src={meetups} alt="" />
-              <div className={classes.after}>
-                <p className={classes.view}>View</p>
+              <div>
+                <i className="fa fa-github" />
+                <a href={item.link}>{item.title}</a>
               </div>
-            </div>
-            <div>
-              <i className="fa fa-github" />
-              <a href="https://github.com/MilneS/meetup">Meetup</a>
-            </div>
-            <div>React</div>
-          </li>
-          <li>
-            <div className={classes.imgcontainer}>
-              <img src={quotes} alt="" />
-              <div className={classes.after}>
-                <p className={classes.view}>View</p>
-              </div>
-            </div>
-            <div>
-              <i className="fa fa-github" />
-              <a href="https://github.com/MilneS/quote">Quote</a>
-            </div>
-            <div>React</div>
-          </li>
-          <li>
-            <div className={classes.imgcontainer}>
-              <img src={expensesChart} alt="" />
-              <div className={classes.after}>
-                <p className={classes.view}>View</p>
-              </div>
-            </div>
-            <div>
-              <i className="fa fa-github" />
-              <a href="https://github.com/MilneS/expenses-chart">
-                Expenses Chart
-              </a>
-            </div>
-            <div>React</div>
-          </li>
-          <li>
-            <div className={classes.imgcontainer}>
-              <img src={colorGame} alt="" />
-              <div className={classes.after}>
-                <p className={classes.view}>View</p>
-              </div>
-            </div>
-            <div>
-              <i className="fa fa-github" />
-              <a href="https://github.com/MilneS/color-game">Color Game</a>
-            </div>
-            <div>Javascript</div>
-          </li>
-          <li>
-            <div className={classes.imgcontainer}>
-              <img src={yelpCamp} alt="" />
-              <div className={classes.after}>
-                <p className={classes.view}>View</p>
-              </div>
-            </div>
-            <div>
-              <i className="fa fa-github" />
-              <a href="https://github.com/MilneS/yelp-camp">YelpCamp</a>
-            </div>
-            <div>Javascript</div>
-          </li>
+              <div>{item.description}</div>
+            </li>
+          ))}
         </ul>
       </div>
       <div>
---
+++ src/comps/Projectsdata.js
@@ -0,0 +1,54 @@
+import foodOrder from "./projectspics/foodorder.png";
+import meetups from "./projectspics/meetups.png";
+import quotes from "./projectspics/quotes.png";
+import expensesChart from "./projectspics/expenseschart.png";
+import colorGame from "./projectspics/colorgame.png";
+import yelpCamp from "./projectspics/yelpcamp.png";
+
+const ProjectsData = [
+  {
+    pic: foodOrder,
+    title: "Food Order app",
+    description: "React",
+    link: "https://github.com/MilneS/food-order-app",
+    id: '1'
+  },
+  {
+    pic: meetups,
+    title: "Meetup",
+    description: "React",
+    link: "https://github.com/MilneS/meetup",
+    id: '2'
+  },
+  {
+    pic: quotes,
+    title: "Quote",
+    description: "React",
+    link: "https://github.com/MilneS/quote",
+    id: '3'
+  },
+  {
+    pic: expensesChart,
+    title: "Expenses Chart",
+    description: "React",
+    link: "https://github.com/MilneS/expenses-chart",
+    id: '4'
+  },
+  {
+    pic: colorGame,
+    title: "Color Game",
+    description: "Javascript",
+    link: "https://github.com/MilneS/color-game",
+    id: '5'
+  },
+
+  {
+    pic: yelpCamp,
+    title: "YelpCamp",
+    description: "Javascript",
+    link: "https://github.com/MilneS/yelp-camp",
+    id: '6'
+  },
+];
+
+export default ProjectsData;
