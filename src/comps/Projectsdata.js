9f5ae9621715087fb5c67f6bb8f8910b018c4c9a
Samira Milne <milne.samira@GMAIL.COM>
2021-09-02 16:49:15 -0500

    projects order

--- src/comps/Projectsdata.js
+++ src/comps/Projectsdata.js
@@ -6,41 +6,43 @@ import colorGame from "./projectspics/colorgame.png";
 import miamyum from "./projectspics/miamyum.png";
 
 const ProjectsData = [
+    {
+    pic: miamyum,
+    title: "Miamyum",
+    description: "React",
+    link: "https://milnes.github.io/miamyum/",
+    id: '1'
+  },
   {
     pic: missing,
     title: "Missing Persons",
     description: "React",
     link: "https://milnes.github.io/missing/",
-    id: '1'
-  },
-  {
-    pic: miamyum,
-    title: "Miamyum",
-    description: "React",
-    link: "https://milnes.github.io/miamyum/",
     id: '2'
+  },  
+  {
+    pic: foodOrder,
+    title: "DoorMeals",
+    description: "React",
+    link: "https://milnes.github.io/DoorMeals/",
+    id: '3'
   },
+
   {
     pic: colorGame,
     title: "Color Game",
     description: "Javascript",
     link: "https://milnes.github.io/color-game/",
-    id: '3'
+    id: '4'
   },
   {
     pic: jokes,
     title: "Jokes on You",
     description: "React",
     link: "https://milnes.github.io/jokes",
-    id: '4'
-  },
-  {
-    pic: foodOrder,
-    title: "DoorMeals",
-    description: "React",
-    link: "https://milnes.github.io/DoorMeals/",
     id: '5'
   },
+
   {
     pic: pokedex,
     title: "Pokédex",
